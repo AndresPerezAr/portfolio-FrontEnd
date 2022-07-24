070e73b2a1a4f8cd343d22940071b2fc1936a70a
Andrés <andresperezar30@gmail.com>
2022-07-24 19:00:08 -0300

    Idiomas

--- front-Angular/src/app/components/skills/skills.component.ts
+++ front-Angular/src/app/components/skills/skills.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { PortfolioService } from 'src/app/servicios/portfolio.service';
 
 @Component({
   selector: 'app-skills',
   templateUrl: './skills.component.html',
-  styleUrls: ['./skills.component.css']
+  styleUrls: ['./skills.component.css'],
 })
 export class SkillsComponent implements OnInit {
+  habilidadesList: any;
+  idiomasList: any;
 
-  constructor() { }
+  constructor(private datosPortfolio: PortfolioService) {}
 
   ngOnInit(): void {
+    this.datosPortfolio.obtenerDatos().subscribe((data) => {
+      this.habilidadesList = data.habilidad;
+      this.idiomasList = data.idioma;
+    });
   }
-
 }
