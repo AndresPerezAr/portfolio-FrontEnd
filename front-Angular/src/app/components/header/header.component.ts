9f8b98b05f7dcbacb513b2ed4a38c260e50171f1
Andrés <andresperezar30@gmail.com>
2022-07-23 23:58:17 -0300

    Fontawesome y servicios corriendo

--- front-Angular/src/app/components/header/header.component.ts
+++ front-Angular/src/app/components/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { PortfolioService } from 'src/app/servicios/portfolio.service';
 
 @Component({
   selector: 'app-header',
@@ -8,9 +9,11 @@ import { Component, OnInit } from '@angular/core';
 export class HeaderComponent implements OnInit {
   hashtag: string = '#YoProgramo';
 
-  constructor() {}
+  constructor(private datosPortfolio: PortfolioService) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.datosPortfolio.obtenerDatos();
+  }
 
   toggleAddTask() {
     console.log('toggle!');
